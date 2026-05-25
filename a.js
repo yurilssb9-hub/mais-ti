8763d6b7f837c19229fee3fdd9af15f7464db84e
yurilssb9-hub <yurilssb9@gmail.com>
2026-05-25 11:21:45 -0300

    Delete a.js

--- a.js
+++
@@ -1,9 +0,0 @@
-const prompt = require ("prompt-sync")();
-
-// Faça um script que leia os dados de uma turma de alunos. 
-// Primeiro deverá ler o número de alunos. 
-// Depois lerá os nomes de todos os alunos. 
-// Finalmente lerá as notas finais de cada um dos alunos. 
-// O programa deverá imprimir os nomes dos alunos que foram aprovados. 
-// Considera-se aprovado o aluno com nota maior ou igual a cinco.
-
