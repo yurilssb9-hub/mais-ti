1497fdd2d158e8e73b927e968877daadc491f92b
yurilssb9-hub <yurilssb9@gmail.com>
2026-06-10 09:19:17 -0300

    Define database schema for airline tickets
    Create tables for Companhia, Trecho, and Cupom in the database.

--- Meus Codigos/dados/passagens-aereas/database.js
+++ Meus Codigos/dados/passagens-aereas/database.js
@@ -3,7 +3,31 @@ const Database = require('better-sqlite3');
 const db = new Database('sistema_passagens.db');
 
 db.exec(`
-CREATE TABLE ()
+    CREATE TABLE Companhia(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        anoFundacao INTEGER NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXIST Trecho(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        idCompanhia INTEGER NOT NULL,
+        origem TEXT NOT NULL,
+        destino TEXT NOT NULL,
+        valor REAL NOT NULL,
+        numeroPassagens INTEGER NOT NULL,
+        FOREIGN KEY (idCompanhia) REFERENCES Companhia(id)
+    );
+
+    CREATE TABLE IF NOT EXIST Cupom (
+        ide INTEGER PRIMARY KEY AUTOINCREMENT,
+        idCompanhia INTEGER NOT NULL,
+        codigo TEXT NOT NULL,
+        percentualDesconto REAL NOT NULL,
+        numeroCupons INTEGER NOT NULL,
+        FOREIGN KEY (idCompanhia) REFERENCES Companhia(id)
+
+    );
 `);
 
 module.exports = db;
